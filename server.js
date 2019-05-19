329f36e5883a7edd9ff66bef8c64f7cd3580bd4f
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-05-19 14:20:39 -0700

    survey logic

--- server.js
+++ server.js
@@ -33,15 +33,15 @@ var friend = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "public/home.html"));
 });
 
 app.get("/home", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
+  res.sendFile(path.join(__dirname, "public/home.html"));
 });
 
 app.get("/survey", function(req, res) {
-  return res.json(characters);
+  res.sendFile(path.join(__dirname, "public/survey.html"));
 });
 
 // Displays all characters
