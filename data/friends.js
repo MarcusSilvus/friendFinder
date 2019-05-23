27bfc0f501a1522bf9648964191c5410ea0d5311
Mark Silva <marksilva@D-10-19-131-10.dhcp4.washington.edu>
2019-05-22 18:27:19 -0700

    survey complete

--- data/friends.js
+++ data/friends.js
@@ -3,17 +3,17 @@
 var friends = [
   {
     name: "Vlad",
-    photo: "",
+    photo: "http://www4.pictures.zimbio.com/mp/03W6Kp8kHTBx.jpg",
     scores: [2, 4, 3, 3, 2, 5, 2, 3, 1, 3]
   },
   {
     name: "Floyd",
-    photo: "",
+    photo: "http://hammondlaw.net/wp-content/uploads/parser/dumb-and-dumber-haircut-1.jpg",
     scores: [5, 4, 5, 5, 3, 4, 4, 4, 5, 3]
   },
   {
     name: "Aughra",
-    photo: "",
+    photo: "https://vignette.wikia.nocookie.net/darkcrystal/images/c/cf/Aughra_photo.jpg/revision/latest?cb=20080528154506",
     scores: [1, 1, 5, 1, 1, 2, 5, 1, 5, 1]
   },
   {
--- server.js
+++ server.js
@@ -7,66 +7,10 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Links to Routes
 require("./routing/apiRoutes.js")(app)
 require("./routing/htmlRoutes.js")(app)
-// Links to Routes
-// to htmlRoutes.js
-
-// to apiRoutes.js
-
-var friend = [
-  {
-    name: "Vlad",
-    photo: "",
-    scores: [2, 4, 3, 3, 2, 5, 2, 3, 1, 3]
-  },
-  {
-    name: "Floyd",
-    photo: "",
-    scores: [5, 4, 5, 5, 3, 4, 4, 4, 5, 3]
-  },
-  {
-    name: "Aughra",
-    photo: "",
-    scores: [1, 1, 5, 1, 1, 2, 5, 1, 5, 1]
-  },
-  {
-    name: "MC Pee Pants",
-    photo: "https://i.ytimg.com/vi/diybDrSohHI/hqdefault.jpg",
-    scores: [3, 3, 5, 1, 5, 2, 5, 1, 5, 1]
-  }
-];
-
-// Basic route that sends the user first to the AJAX Page
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "public/home.html"));
-// });
-
-// //var home = require('/public/home.html');
-
-// app.get("/home", function(req, res) {
-//   res.sendFile(path.join(__dirname, "public/home.html"));
-// });
-
-// app.get("/survey", function(req, res) {
-//   res.sendFile(path.join(__dirname, "public/survey.html"));
-// });
-
-// app.get("/api/friends", function(req, res) {
-//   return res.json(friend);
-// });
-
-// // Create New Friend - takes in JSON input
-// app.post("/api/friends", function(req, res) {
-  
-//   var newFriend = req.body;
-
-//   console.log(newFriend);
-
-//   friend.push(newFriend);
-
-//   res.json(newFriend);
-// });
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
