5e53cd102a33b9c95495c76a4562a85f87f5717a
Mark Silva <marksilva@Marks-MBP.localdomain>
2019-05-25 14:21:46 -0700

    building out match logic

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -14,12 +14,32 @@ module.exports = function(app) {
       photo: "",
       friendDiff: 1000
     };
-    var newFriend = req.body;
-  
-    console.log(newFriend);
-  
-    friend.push(newFriend);
-  
-    res.json(newFriend);
+    var userData = req.body;
+    var userName = userData.name;
+    var userScores = userData.scores;
+
+    var b = userScores.map(function(item) {
+      return parseInt(item, 10);
+    });
+    userData = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: b
+    };
+    
+    console.log("Name: " + userName);
+    console.log("User score: " + userScores);
+
+    var sum = b.reduce((a, b) => a + b, 0);
+    console.log("Sum of user's score: " + sum);
+    console.log("Best match: " + bestMatch.friendDiff);
+    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    
+    
   }); 
 }
+
+
+  // friend.push(newFriend);
+  
+    // res.json(newFriend);
