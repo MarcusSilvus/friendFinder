dfe7a0ff900588915fdd8997ca13755d40591ddf
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-05-28 19:27:01 -0700

    modal working

--- data/friends.js
+++ data/friends.js
@@ -14,7 +14,7 @@ var friends = [
   {
     name: "Aughra",
     photo: "https://vignette.wikia.nocookie.net/darkcrystal/images/c/cf/Aughra_photo.jpg/revision/latest?cb=20080528154506",
-    scores: [1, 1, 5, 1, 1, 2, 5, 1, 5, 1]
+    scores: [1, 2, 5, 1, 1, 2, 5, 1, 5, 1]
   },
   {
     name: "MC Pee Pants",
@@ -24,58 +24,3 @@ var friends = [
 ];
 
 module.exports = friends;
-
-// for (var i = 0; i < friend.length; i++) {
-//   var person = friend[i];
-//   console.log(person.name);
-  
-// }
-
-// var friend1 = {
-//   name: "Test",
-//   scores: [4, 4, 3, 1, 2, 5, 5, 4, 1, 2]
-// };
-
-// var friend2 = {
-//  name: "Vlad",
-//  scores: [2, 4, 3, 3, 2, 5, 2, 3, 1, 3]
-// };
-
-// var friends = [
-// {
-//   name: "donnie",
-//   scores: [3, 5, 3, 5, 2, 4, 2, 5, 3, 2]
-// },
-// {
-//   name: "samson",
-//   scores: [4, 4, 4, 4, 4, 4, 2, 4, 4, 2]
-// }
-// ];
-
-// var userScore1 = friend1.scores;
-// var userScore2 = friend2.scores;
-
-// var userTotal1 = 0;
-// var userTotal2 = 0;
-
-// for (var i = 0; i < userScore1.length; i++) {
-// userTotal1 += userScore1[i];
-// //console.log(userTotal1)
-// }
-
-// for (var i = 0; i < userScore2.length; i++) {
-// userTotal2 += userScore2[i];
-// //console.log(userTotal2)
-// }
-
-// for (var i = 0; i < friends.length; i++) {
-// var friendsArr = 0;
-// var friendsScore = friends[i].scores;
-// //console.log(friendsScore);
-// for (var j = 0; j < friendsScore.length; j++) {
-//   friendsArr += friendsScore[j];
-// console.log(friendsArr)
-// }
-// }
-
-// console.log(Math.abs(userTotal2 - userTotal1));
